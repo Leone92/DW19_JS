0f82f7562739975acb50197fb9aa4511fb60384f
Leone <ilyesharouni20@gmail.com>
2022-06-27 14:35:23 +0200

    ajout com + exo

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.error("test");
+/* 
+Commentaire:
+nous allons apprendre:
+
+1 les variables
+2 les conditions
+3 les boucles
+4 les fonctions
+*/
+
+/* Les variables :
+Pourquoi les variables ? pour stocker des donées ex: nom d'un jeu - "jeu du plus ou du moins" 
+une variable est constituer de 2 choses : le nom de la variable et la valeur de la variable
+ex : tiroir A <= 10 Valeur
+
+Le numerique ne contient pas de guillemet
+les chaines de caracteres contiennent des guillemets
+Additionner des nombres sa fonctionne
+Additionner une chaine de caractere avec une autre choses fait de la concaténation (enchainement de termes)
+
+*/
+a=10 /* 10 rentre dans la variable A*/
+b=50 /* Pareil*/
+c="80" /* 10 Pareil A*/
+b=90 /* B va contenir 90*/
+
+console.error(a+b);
+console.error(typeof(c));
+console.error(a+parseInt(c));
+
+/* EXO les variables*/
+
+SEL=100;
+SUCRE=80;
+console.error("Nous avons du SEL en quantité de " + SEL + ". Nous avons du sucre en quantité de " + SUCRE)
+/* Le code affichera du Sel en quantité de 100. Nous avons du sucre en quantité de 80*/
+/*  */
+
+console.error("Nous avons du SEL en quantité de " + SEL + ". Nous avons du sucre en quantité de " + SUCRE)
